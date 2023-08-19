f35f7d5f0f69f3b6c513f75ec7e5df441b673b00
Meganathan <meganathan212003@gmail.com>
2023-08-19 19:25:58 +0530

    add: added the new java file

--- src/DSA/InsertionSortExample.java
+++ src/DSA/InsertionSortExample.java
@@ -1,7 +1,43 @@
 package DSA;
 
-public class InsertionSortExample {
-    public static void main(String[] args) {
+/**
+ * Below the code for DSA Insertion sorting this sorting is very efficent and
+ * very use full and this also took the less complexcity
+ * 
+ * @author MeganathanSubrmanian
+ */
 
+class insertionSort {
+    public static void insertionSortExample(int[] arr)
+    {
+        // below the line for
+        int x = 0, i;
+
+        for (int j = 1; j < arr.length; j++)
+        {
+            x = arr[j]; // x=5
+            i = j - 1; // i=0
+            while (i >= 0 && x <= arr[i]) 
+            {
+                arr[i + 1] = arr[i];
+                i--;
+            }
+            arr[i + 1] = x;
+        }
+
+        int y = 0;
+
+        for (y = 0; y < arr.length; y++)
+            System.out.println(arr[y]);
+
+    }
+}
+
+public class insertionSortExample {
+    public static void main(String[] args)
+    {
+        int[] arr = { 4, 5, 12, 8, 7, 6, 22, 1 };
+
+        insertionSort.insertionSortExample(arr);
     }
 }
